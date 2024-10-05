8b4d17122d114a3a720316f1da8c47cb7bc5b5f0
mrahman141 <mrahman141@myseneca.ca>
2024-10-05 19:21:12 -0400

    Bug fix

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -8,7 +8,7 @@ const { Fragment } = require('../../model/fragment');
  */
 module.exports.getall = async (req, res) => {
   try {
-    const expand = req.query.expand === '0' ? false : true;
+    const expand = req.query.expand === '1';
 
     const result = await Fragment.byUser(req.user, expand);
     const data = { fragments: result ? [result] : [] };
